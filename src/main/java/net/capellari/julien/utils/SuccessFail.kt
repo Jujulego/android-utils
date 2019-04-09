95e39057216d007ce408fd5d4d8cfb72c1558906
Julien Capellari <julien@capellari.net>
2019-04-09 19:28:08 +0200

    add: succeed and failed methods

--- src/main/java/net/capellari/julien/utils/SuccessFail.kt
+++ src/main/java/net/capellari/julien/utils/SuccessFail.kt
@@ -7,16 +7,25 @@ fun <SP: Any?, FP: Any?> success(cb: (SP) -> Unit) = Success<SP,FP>(cb)
 // Classes
 open class SuccessFail<in SP: Any?, in FP: Any?>(success: (SP) -> Unit, fail: (FP) -> Unit) {
     // Attributs
-    val _success = success
-    val _fail = fail
+    internal val cbSuccess = success
+    internal val cbFail = fail
+
+    // Méthodes
+    fun succeed(arg: SP) {
+        cbSuccess(arg)
+    }
+
+    fun failed(arg: FP) {
+        cbFail(arg)
+    }
 }
 
 class Success<in SP: Any?, FP: Any?>(success: (SP) -> Unit): SuccessFail<SP,FP>(success, {}) {
     // Opérateurs
-    operator fun invoke(arg: SP) = _success(arg)
+    operator fun invoke(arg: SP) = cbSuccess(arg)
 
     // Méthodes
     infix fun fail(cb: (FP) -> Unit): SuccessFail<SP,FP> {
-        return SuccessFail(_success, cb)
+        return SuccessFail(cbSuccess, cb)
     }
 }
